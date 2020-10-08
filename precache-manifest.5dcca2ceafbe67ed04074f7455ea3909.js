8d84e007c4ecf5934d90e95524a76b43e6a27416
Bryan Simca <bryansimca20@gmail.com>
2020-10-08 01:00:57 -0700

    Updates

---
+++ precache-manifest.5dcca2ceafbe67ed04074f7455ea3909.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65314f791e0646dfadee43eeaa661ecf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "508178bf6412e145aa28",
+    "url": "/portfolio/static/css/2.962a8bb0.chunk.css"
+  },
+  {
+    "revision": "595da1c480fade4311b0",
+    "url": "/portfolio/static/css/main.cbd33c03.chunk.css"
+  },
+  {
+    "revision": "508178bf6412e145aa28",
+    "url": "/portfolio/static/js/2.422031f5.chunk.js"
+  },
+  {
+    "revision": "49d543986fb36111f0dc0c4b45f8010b",
+    "url": "/portfolio/static/js/2.422031f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595da1c480fade4311b0",
+    "url": "/portfolio/static/js/main.05295f9f.chunk.js"
+  },
+  {
+    "revision": "cbc5c7d2f0629954e339",
+    "url": "/portfolio/static/js/runtime-main.2fc27b3b.js"
+  },
+  {
+    "revision": "9a9ea84e73160791e946a09f92991d41",
+    "url": "/portfolio/static/media/Book_Stack.9a9ea84e.svg"
+  },
+  {
+    "revision": "08677cba82a1b5127d6a2d16e30d6d14",
+    "url": "/portfolio/static/media/LinkedIn_Photo.08677cba.JPG"
+  },
+  {
+    "revision": "107d6402f273bb1878ee8c945d49f3fa",
+    "url": "/portfolio/static/media/UT-small.107d6402.jpg"
+  }
+]);
